12c30e24461c82a57a846debc497c6acfe182b0b
arafat_ratul <yeasirarafat.ratul@gmail.com>
2020-03-14 00:02:17 +0600

    function

--- practice.dart
+++ practice.dart
@@ -1,6 +1,5 @@
 // DART is an Object Oriented Programming Language and everything is an object
 
-
 // ---------COMMENTS---------- //
 
 // comments in -line
@@ -11,7 +10,6 @@ block comment
 
 /// documentation
 
-
 // ---------DATA TYPES---------- //
 /* 
 int
@@ -20,21 +18,18 @@ String
 bool
 dynamic
 */
+main() {
+  int integer = 100;
+  var value = 300;
 
-int integer = 100;
-var value = 300;
+  double dbl = 300.44;
+  String string = "Hello World";
+  print('$string');
+  bool isItTrue = true;
+  dynamic weakVarible = "nothing"; //value can be changed at runtime
 
-double dbl = 300.44;
-String string = "Hello World";
-
-bool isItTrue = true;
-dynamic weakVarible = "nothing"; //value can be changed at runtime
-
-weakVariable = 100;
-print('$weakVaribale  \n');
-
-
-// ---------TYPE CONVERSION---------- //
+// weakVariable = 100;
+// print('$weakVaribale');
 
 // ---------STRING---------- //
 
@@ -42,22 +37,14 @@ print('$weakVaribale  \n');
 for RAW string you have to write like this
 
 */
-var s = r'In a raw string, nob even \n gets special treatment';
+  var s = r'In a raw string, nob even \n gets special treatment';
 
+// ---------TYPE CONVERSION---------- //
+
+  var one = int.parse('1');
+  assert(one == 1);
+  var onePointOne = double.parse('1.1');
+  assert(onePointOne == 1.1);
+}
 
 // ---------FUNCTION---------- //
-
-import 'dart:io';
-
-main() {
-  var firstname = "Arafat";
-  String lastname = "123";
-  const age = 33;
-//   print(firstname + ' ' + lastname + ' ' + age);
-  stdout.writeln("what is your name:?");
-  // take input
-
-  String name = stdin.readLineSync();
-  // String Interpolation
-  print("My name is: $name");
-}
