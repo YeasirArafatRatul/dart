c9b48a766d2ee8f30bae702fdf3f112daa71b2f8
arafat_ratul <yeasirarafat.ratul@gmail.com>
2020-03-16 12:50:24 +0600

    readme

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 Some-basic practices of Dart-Programming Language
 
-## it is very much readble and easy programming language to learn, anyone with experience in other programming languages like C,C++,Java,Python,JavaScript can understand it at glance. 
+## it is very much readble and easy programming language to learn, anyone with experience in other programming languages like C,C++,Java,Python,JavaScript can understand it at a glance. 
--- function.dart
+++ function.dart
@@ -4,12 +4,38 @@ dynamic add(var a, var b) {
   return a + b;
 }
 
+// function with named parameter
+dynamic div({var num1, var num2}) {
+  return num1 / num2;
+}
+
 void main() {
   show("Hello");
   print(add(3, 4));
+  print(mul(2, 3));
   print(add('3', '4'));
+
+  print(div(num2: 4, num1: 3)); //named arguments
+
+  // anonymous/lambda/closure function
+  var list = ['a', 'b', 'c'];
+  list.forEach((item) {
+    print(item);
+  });
 }
+
+// without return statement
+dynamic mul(var num1, var num2) => num1 * num2;
 
 void show(var msg) {
   print(msg);
 }
+
+void printf(item) {
+  print(item);
+}
+
+/* there are two types of parameters in DART
+1.Positional
+2. Named
+*/
